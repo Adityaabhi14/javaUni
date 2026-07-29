13b8810a4b890b0f8e355d1d970a88746651c428
Aditya <aditya.techie1407@gmail.com>
2026-07-29 12:42:39 +0530

    NumTable

---
+++ NumTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class NumTable {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a;
+		System.out.println("Enter a Number that you want the Table");
+		a = scanner.nextInt();
+		for(int i = 1;i < 11; i++) {
+			System.out.println(a+ " x " + i + " = " + a*i);
+			}
+		scanner.close();
+ 
+	}
+}
